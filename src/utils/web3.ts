f4caf791c6163127bc17576f9ad24849f4ebc51d
Denis Vybornyj <vybornyj@users.noreply.github.com>
2021-05-06 15:04:03 +0300

    feat: implemented new uikit methods for cross-domain auth

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,10 +1,9 @@
 import Web3 from 'web3'
 import { HttpProviderOptions } from 'web3-core-helpers'
 import getRpcUrl from 'utils/getRpcUrl'
+import { getChainId } from '@alium-official/uikit'
 
-const params = new URLSearchParams(window.location.search)
-const id = params.get('network')
-const chainId: any = id || parseInt(process.env.REACT_APP_CHAIN_ID as string, 10)
+const chainId = getChainId()
 const RPC_URL = getRpcUrl(chainId)
 const httpProvider = new Web3.providers.HttpProvider(RPC_URL, { timeout: 10000 } as HttpProviderOptions)
 const web3NoAccount = new Web3(httpProvider)
