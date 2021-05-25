ca92a53d2e7b04671550c8e3017d82810b01aae8
sieugene <sieugene@mail.ru>
2021-05-25 17:42:14 +0300

    change date, add gtm

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import Providers from './Providers'
-import './i18n'
+import TagManager from 'react-gtm-module'
 import 'typeface-roboto'
+import App from './App'
+import './i18n'
+import Providers from './Providers'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-MWZ3WL5',
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
   <React.StrictMode>
