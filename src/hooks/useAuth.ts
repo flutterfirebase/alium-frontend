b89ad56562511fd719cc97e6d30ee1e61b0ce896
sieugene <sieugene@mail.ru>
2021-05-27 16:56:29 +0300

    Make login async for connection load

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,13 +1,13 @@
-import { ConnectorNames,removeConnectorId } from '@alium-official/uikit'
+import { ConnectorNames, removeConnectorId } from '@alium-official/uikit'
 import { NoBscProviderError } from '@binance-chain/bsc-connector'
-import { UnsupportedChainIdError,useWeb3React } from '@web3-react/core'
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core'
 import {
-NoEthereumProviderError,
-UserRejectedRequestError as UserRejectedRequestErrorInjected
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
 } from '@web3-react/injected-connector'
 import {
-UserRejectedRequestError as UserRejectedRequestErrorWalletConnect,
-WalletConnectConnector
+  UserRejectedRequestError as UserRejectedRequestErrorWalletConnect,
+  WalletConnectConnector,
 } from '@web3-react/walletconnect-connector'
 import { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
@@ -22,40 +22,44 @@ const useAuth = () => {
   const dispatch = useDispatch()
 
   const login = useCallback(
-    (connectorID: ConnectorNames) => {
-      const { chainId, connector } = getConnectorsByName(connectorID)
-      if (connector) {
-        activate(connector, async (error: Error) => {
-          if (error instanceof UnsupportedChainIdError) {
-            const hasSetup = await setupNetwork(chainId)
-            if (hasSetup) {
-              try {
-                activate(connector, (err) => console.error('err :>> ', err))
-              } catch (err) {
-                console.error('err :>> ', err)
+    async (connectorID: ConnectorNames) => {
+      try {
+        const { chainId, connector } = getConnectorsByName(connectorID)
+        if (connector) {
+          activate(connector, async (error: Error) => {
+            if (error instanceof UnsupportedChainIdError) {
+              const hasSetup = await setupNetwork(chainId)
+              if (hasSetup) {
+                try {
+                  activate(connector, (err) => console.error('err :>> ', err))
+                } catch (err) {
+                  console.error('err :>> ', err)
+                }
               }
-            }
-          } else {
-            removeConnectorId()
-            if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
-              // toastError('Provider Error', 'No provider was found')
-            } else if (
-              error instanceof UserRejectedRequestErrorInjected ||
-              error instanceof UserRejectedRequestErrorWalletConnect
-            ) {
-              if (connector instanceof WalletConnectConnector) {
-                const walletConnector = connector as WalletConnectConnector
-                walletConnector.walletConnectProvider = null
-              }
-              // toastError('Authorization Error', 'Please authorize to access your account')
             } else {
-              dispatch(setConnectionError({ error }))
-              // toastError(error.name, error.message)
+              removeConnectorId()
+              if (error instanceof NoEthereumProviderError || error instanceof NoBscProviderError) {
+                // toastError('Provider Error', 'No provider was found')
+              } else if (
+                error instanceof UserRejectedRequestErrorInjected ||
+                error instanceof UserRejectedRequestErrorWalletConnect
+              ) {
+                if (connector instanceof WalletConnectConnector) {
+                  const walletConnector = connector as WalletConnectConnector
+                  walletConnector.walletConnectProvider = null
+                }
+                // toastError('Authorization Error', 'Please authorize to access your account')
+              } else {
+                dispatch(setConnectionError({ error }))
+                // toastError(error.name, error.message)
+              }
             }
-          }
-        })
-      } else {
-        // toastError("Can't find connector", 'The connector config is wrong')
+          })
+        } else {
+          // toastError("Can't find connector", 'The connector config is wrong')
+        }
+      } catch (error) {
+        dispatch(setConnectionError({ error }))
       }
     },
     [activate],
